6de45eb13ed48d1027b871072f9fb65337277b91
Job Céspedes Ortiz <jobcespedes@gmail.com>
2023-06-13 08:06:47 -0600

    build: bump module

--- modules/node/butane.tf
+++ modules/node/butane.tf
@@ -1,6 +1,6 @@
 module "butane_k3s_snippets" {
   source  = "krestomatio/butane-snippets/ct//modules/k3s"
-  version = "0.0.31"
+  version = "0.0.32"
 
   config            = var.k3s_config
   mode              = var.mode
